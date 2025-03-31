80be35d31afd75b4727f43d1aa04f56565a06d42
hana <hanaabrham89@gmail.com>
2025-03-31 11:00:28 +0200

    update MainNavbar styles: change active link color to red and add gradient background

--- src/features/navigation/MainNavbar.tsx
+++ src/features/navigation/MainNavbar.tsx
@@ -1,13 +1,13 @@
-import { NavLink } from "react-router";
+import { NavLink } from "react-router-dom";
 
 function MainNavbar() {
   return (
     <div className="flex justify-center">
-      <nav className="flex justify-center space-x-8 border p-2 mb-2 rounded-3xl w-xl">
+      <nav className="flex justify-center space-x-8 border p-2 mb-2 rounded-3xl w-xl bg-gradient-to-br from-[#00d4ff] to-[#4caf50]">
         <NavLink
           to="/"
           className={({ isActive }) =>
-            isActive ? "text-green-500" : "text-black"
+              isActive ? "text-red-500" : "text-black"
           }
         >
           Home
@@ -15,7 +15,7 @@ function MainNavbar() {
         <NavLink
           to="/about-me"
           className={({ isActive }) =>
-            isActive ? "text-green-500" : "text-black"
+            isActive ? "text-red-500" : "text-black"
           }
         >
           About Me
@@ -23,7 +23,7 @@ function MainNavbar() {
         <NavLink
           to="/projects"
           className={({ isActive }) =>
-            isActive ? "text-green-500" : "text-black"
+             isActive ? "text-red-500" : "text-black"
           }
         >
           Projects
@@ -31,7 +31,7 @@ function MainNavbar() {
         <NavLink
           to="/skills"
           className={({ isActive }) =>
-            isActive ? "text-green-500" : "text-black"
+           isActive ? "text-red-500" : "text-black"
           }
         >
           Skills
@@ -39,7 +39,7 @@ function MainNavbar() {
         <NavLink
           to="/get-in-touch"
           className={({ isActive }) =>
-            isActive ? "text-green-500" : "text-black"
+             isActive ? "text-red-500" : "text-black"
           }
         >
           Get in Touch
